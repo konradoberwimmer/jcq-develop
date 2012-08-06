b71141db6c8c2fb834feaae19c7fe3fe8e40db19
DAEMONK <DAEMONK@e984a57e-3a46-f59d-375c-d0b27d5bb152>
2012-08-06 16:27:40 +0000

    WORK IN PROGRESS, DONT CHECK OUT!

---
+++ trunk/administrator/components/com_jcq/toolbar.jcq.php
@@ -0,0 +1,13 @@
+<?php
+defined( '_JEXEC' ) or die( 'Restricted access' );
+require_once( JApplicationHelper::getPath( 'toolbar_html' ) );
+switch($task)
+{
+	case 'edit':
+	case 'add':
+		TOOLBAR_jcq::_NEW();
+		break;
+	default:
+		TOOLBAR_jcq::_DEFAULT();
+		break;
+}
---
+++ trunk/administrator/components/com_jcq/views/projectlist/view.php
@@ -0,0 +1,13 @@
+<?php
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.view');
+
+class jcqadminViewProjectlist extends JView
+{
+
+	function display()
+	{
+		echo "Greetings, everything is ok!";
+	}
+}
