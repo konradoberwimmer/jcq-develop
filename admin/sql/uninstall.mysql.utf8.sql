1ac5c4a1a5acadd512d54ccc8b1fa072880d65c0
DAEMONK <DAEMONK@e984a57e-3a46-f59d-375c-d0b27d5bb152>
2013-03-21 10:50:44 +0000

    transition to offline development

---
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,8 @@
+DROP TABLE jcq_itemscales;
+DROP TABLE jcq_questionscales;
+DROP TABLE jcq_item;
+DROP TABLE jcq_question;
+DROP TABLE jcq_page;
+DROP TABLE jcq_project;
+DROP TABLE jcq_code;
+DROP TABLE jcq_scale;
