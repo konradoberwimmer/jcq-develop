8c6c2a44c1736a660580ceac585e8034e75af41d
DAEMONK <DAEMONK@e984a57e-3a46-f59d-375c-d0b27d5bb152>
2013-03-25 17:38:46 +0000

    added uninstall code

---
+++ script.php
@@ -0,0 +1,45 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.application.component.model');
+
+class com_jcqInstallerScript
+{
+	function install($parent)
+	{
+		
+	}
+
+	function uninstall($parent)
+	{
+		$db = JFactory::getDbo();
+		$db->setQuery('SELECT * FROM jcq_project');
+		$projects = $db->loadObjectList();
+		foreach ($projects as $project)
+		{
+			$query = "DROP TABLE jcq_proj".$project->ID;
+			$db->setQuery($query);
+			if (!$db->query()){
+				$errorMessage = JFactory::getDbo()->getErrorMsg();
+				JError::raiseError(500, 'Error deleting projects: '.$errorMessage);
+			}
+		}
+	}
+
+	
+	function update($parent)
+	{
+	
+	}
+
+	function preflight($type, $parent)
+	{
+	
+	}
+
+	function postflight($type, $parent)
+	{
+	
+	}
+}
