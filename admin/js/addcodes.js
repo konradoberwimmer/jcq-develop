8b08408e334d53c13ca30c1e10ac1885796ddf8d
DAEMONK <DAEMONK@e984a57e-3a46-f59d-375c-d0b27d5bb152>
2013-04-11 11:53:35 +0000

    cleaned some JavaScript-code

--- admin/js/addcodes.js
+++ admin/js/addcodes.js
@@ -1,69 +1,39 @@
 function addCode() 
 {
-	var listscalebody = document.getElementById("listscalebody");
-	var newTR = document.createElement("tr");
-	var newTDord = document.createElement("td");
-	var newTDvalue = document.createElement("td");
-	var newTDlabel = document.createElement("td");
-	var newTDmissval = document.createElement("td");
-	var newTDdelete = document.createElement("td");
+	var table = document.getElementById("listscalebody");
+	var newTR = table.insertRow(table.rows.length);
+	var newTDord = newTR.insertCell(0);
+	var newTDvalue = newTR.insertCell(1);
+	var newTDlabel = newTR.insertCell(2);
+	var newTDmissval = newTR.insertCell(3);
+	var newTDdelete = newTR.insertCell(4);
+	
 	//create field for order
 	var newINPUTcodeord = document.createElement("input");
-	var newINPUTcodeordTYPE = document.createAttribute("type");
-	newINPUTcodeordTYPE.nodeValue = "text";
-	newINPUTcodeord.setAttributeNode(newINPUTcodeordTYPE);
-	var newINPUTcodeordNAME = document.createAttribute("name");
-	newINPUTcodeordNAME.nodeValue = "codeord[]";
-	newINPUTcodeord.setAttributeNode(newINPUTcodeordNAME);
-	var newINPUTcodeordVALUE = document.createAttribute("value");
-	//TODO find lowest available value > 0
-	newINPUTcodeordVALUE.nodeValue = "0";
-	newINPUTcodeord.setAttributeNode(newINPUTcodeordVALUE);
+	newINPUTcodeord.setAttribute("type","text");
+	newINPUTcodeord.setAttribute("name","codeord[]");
+	newINPUTcodeord.setAttribute("value",table.rows.length);
+	newTDord.appendChild(newINPUTcodeord);
+	
 	//create hidden field with id
 	var newINPUTcodeid = document.createElement("input");
-	var newINPUTcodeidTYPE = document.createAttribute("type");
-	newINPUTcodeidTYPE.nodeValue = "hidden";
-	newINPUTcodeid.setAttributeNode(newINPUTcodeidTYPE);
-	var newINPUTcodeidNAME = document.createAttribute("name");
-	newINPUTcodeidNAME.nodeValue = "codeids[]";
-	newINPUTcodeid.setAttributeNode(newINPUTcodeidNAME);
-	var newINPUTcodeidVALUE = document.createAttribute("value");
-	newINPUTcodeidVALUE.nodeValue = "0";
-	newINPUTcodeid.setAttributeNode(newINPUTcodeidVALUE);
-	//create field with value
+	newINPUTcodeid.setAttribute("type","hidden");
+	newINPUTcodeid.setAttribute("name","codeids[]");
+	newINPUTcodeid.setAttribute("value","0");
+	newTDord.appendChild(newINPUTcodeid);
+	
+	//create field with code value
 	var newINPUTcodevalue = document.createElement("input");
-	var newINPUTcodevalueTYPE = document.createAttribute("type");
-	newINPUTcodevalueTYPE.nodeValue = "text";
-	newINPUTcodevalue.setAttributeNode(newINPUTcodevalueTYPE);
-	var newINPUTcodevalueNAME = document.createAttribute("name");
-	newINPUTcodevalueNAME.nodeValue = "codevalue[]";
-	newINPUTcodevalue.setAttributeNode(newINPUTcodevalueNAME);
-	var newINPUTcodevalueVALUE = document.createAttribute("value");
-	newINPUTcodevalueVALUE.nodeValue = "0";
-	newINPUTcodevalue.setAttributeNode(newINPUTcodevalueVALUE);
+	newINPUTcodevalue.setAttribute("type","text");
+	newINPUTcodevalue.setAttribute("name","codevalue[]");
+	newINPUTcodevalue.setAttribute("value",table.rows.length);
+	newTDvalue.appendChild(newINPUTcodevalue);
+	
 	//create field with label
 	var newINPUTcodelabel = document.createElement("input");
-	var newINPUTcodelabelTYPE = document.createAttribute("type");
-	newINPUTcodelabelTYPE.nodeValue = "text";
-	newINPUTcodelabel.setAttributeNode(newINPUTcodelabelTYPE);
-	var newINPUTcodelabelNAME = document.createAttribute("name");
-	newINPUTcodelabelNAME.nodeValue = "codelabel[]";
-	newINPUTcodelabel.setAttributeNode(newINPUTcodelabelNAME);
-	var newINPUTcodelabelVALUE = document.createAttribute("value");
-	newINPUTcodelabelVALUE.nodeValue = "";
-	newINPUTcodelabel.setAttributeNode(newINPUTcodelabelVALUE);
-	var newINPUTcodelabelSIZE = document.createAttribute("size");
-	newINPUTcodelabelSIZE.nodeValue = "128";
-	newINPUTcodelabel.setAttributeNode(newINPUTcodelabelSIZE);
-	
-	newTDord.appendChild(newINPUTcodeord);
-	newTDord.appendChild(newINPUTcodeid);
-	newTR.appendChild(newTDord);
-	newTDvalue.appendChild(newINPUTcodevalue);
-	newTR.appendChild(newTDvalue);
+	newINPUTcodelabel.setAttribute("type","text");
+	newINPUTcodelabel.setAttribute("name","codelabel[]");
+	newINPUTcodelabel.setAttribute("size","128");
+	newINPUTcodelabel.setAttribute("value","");
 	newTDlabel.appendChild(newINPUTcodelabel);
-	newTR.appendChild(newTDlabel);
-	newTR.appendChild(newTDmissval);
-	newTR.appendChild(newTDdelete);
-	listscalebody.appendChild(newTR);
 }
