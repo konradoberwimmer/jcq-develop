c3953e433bb0afa8a0e21bf996aa7c2be113b9a9
DAEMONK <DAEMONK@e984a57e-3a46-f59d-375c-d0b27d5bb152>
2013-09-09 15:13:14 +0000

    prepared for send emails

--- trunk/admin/views/projectlist/view.php
+++ trunk/admin/views/projectlist/view.php
@@ -38,8 +38,9 @@ class JcqViewProjectlist extends JView
 		JHTML::script($path.$filename, true);
 		
 		JToolBarHelper::title('JCQ: Projects', 'generic.png');
+		JToolBarHelper::preferences('com_jcq');
+		JToolBarHelper::divider();
 		if ($projects != null) JToolBarHelper::customX("exportProject","archive.png",".png","Export project",true);
-		if ($projects != null) JToolBarHelper::divider();
 		if ($projects != null) JToolBarHelper::deleteList("Do you really want to delete the selected projects?",'removeProject','Remove');
 		if ($projects != null) JToolBarHelper::editList('editProject','Edit');
 		JToolBarHelper::custom("showImportProject","unarchive.png",".png","Import project",false);
