41cb9ba83bf0593ab78354aa811650bc6140ddf8
DAEMONK <DAEMONK@e984a57e-3a46-f59d-375c-d0b27d5bb152>
2013-09-21 12:53:43 +0000

    added progress bar in frontend

--- site/models/page.php
+++ site/models/page.php
@@ -6,6 +6,13 @@ jimport('joomla.application.component.model');
 class JcqModelPage extends JModel
 {
 	private $pageID = null;
+	private $db;
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->db = $this->getDBO();
+	}
 	
 	function setPage($pageID)
 	{
@@ -14,55 +21,68 @@ class JcqModelPage extends JModel
 	
 	function getPage()
 	{
-		$db = $this->getDBO();
-		$db->setQuery("SELECT * FROM jcq_page WHERE ID=".$this->pageID);
-		return $db->loadObject();
+		$this->db->setQuery("SELECT * FROM jcq_page WHERE ID=".$this->pageID);
+		return $this->db->loadObject();
+	}
+	
+	function getPagePosition()
+	{
+		$this->db->setQuery("SELECT * FROM jcq_page WHERE ID=".$this->pageID);
+		$page = $this->db->loadObject();
+		$this->db->setQuery("SELECT * FROM jcq_page WHERE projectID=".$page->projectID." ORDER BY isFinal, ord");
+		$pages = $this->db->loadObjectList();
+		for ($i=0;$i<count($pages);$i++) if ($pages[$i]->ID == $this->pageID) break;
+		return (float)$i/((float)(count($pages)-1));
+	}
+	
+	function getProjectName()
+	{
+		$this->db->setQuery("SELECT * FROM jcq_page WHERE ID=".$this->pageID);
+		$page = $this->db->loadObject();
+		$this->db->setQuery("SELECT * FROM jcq_project WHERE ID=".$page->projectID);
+		$project = $this->db->loadObject();
+		return $project->name;
 	}
 	
 	//requires the pageID to be set
 	function getQuestions()
 	{
 		$sqlquestions = "SELECT * FROM jcq_question WHERE pageID=".$this->pageID." ORDER BY ord";
-		$db = $this->getDBO();
-		$db->setQuery($sqlquestions);
-		return $db->loadObjectList();
+		$this->db->setQuery($sqlquestions);
+		return $this->db->loadObjectList();
 	}
 	
 	function getScaleToQuestion($questionID)
 	{
 		$query = 'SELECT * FROM jcq_questionscales WHERE questionID = '.$questionID;
-		$db = $this->getDBO();
-		$db->setQuery($query);
-		$scale = $db->loadObjectList();
+		$this->db->setQuery($query);
+		$scale = $this->db->loadObjectList();
 		if ($scale==null) return null;
 		
 		$scaleID = $scale[0]->scaleID;
 		$query = 'SELECT * FROM jcq_code  WHERE scaleID = '.$scaleID.' ORDER BY ord';
-		$db->setQuery($query);
-		return $db->loadObjectList();
+		$this->db->setQuery($query);
+		return $this->db->loadObjectList();
 	}
 	
 	function getScalesToQuestion($questionID)
 	{
 		$query = 'SELECT * FROM jcq_scale, jcq_questionscales WHERE jcq_scale.ID = jcq_questionscales.scaleID AND jcq_questionscales.questionID = '.$questionID.' ORDER BY ord';
-		$db = $this->getDBO();
-		$db->setQuery($query);
-		return $db->loadObjectList();
+		$this->db->setQuery($query);
+		return $this->db->loadObjectList();
 	}
 	
 	function getCodesToScale($scaleID)
 	{
 		$query = 'SELECT * FROM jcq_code WHERE scaleID = '.$scaleID.' ORDER BY ord';
-		$db = $this->getDBO();
-		$db->setQuery($query);
-		return $db->loadObjectList();
+		$this->db->setQuery($query);
+		return $this->db->loadObjectList();
 	}
 	
 	function getItemsToQuestion($questionID)
 	{
-		$db = $this->getDBO();
 		$query = 'SELECT * FROM jcq_item  WHERE questionID = '.$questionID.' ORDER BY ord';
-		$db->setQuery($query);
-		return $db->loadObjectList();
+		$this->db->setQuery($query);
+		return $this->db->loadObjectList();
 	}
 }
