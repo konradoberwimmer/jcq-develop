88b8db427fa941ea92972d7ca635613032dbffba
konrad.oberwimmer@gmail.com <konrad.oberwimmer@gmail.com@e984a57e-3a46-f59d-375c-d0b27d5bb152>
2014-03-14 21:03:33 +0000

    added possibility to use XSLT when importing a project

--- admin/views/importproject/tmpl/importprojectlayout.php
+++ admin/views/importproject/tmpl/importprojectlayout.php
@@ -1,9 +1,13 @@
 <?php
 defined('_JEXEC') or die('Restricted access'); ?>
 <form action="index.php" method="POST" enctype="multipart/form-data" name="adminForm">
-       File: <input type="file" name="file_upload"/><br/>
+<table class="settings">
+<tr><td>XML File</td><td><input type="file" name="file_upload"/></td></tr>
+<tr><td>Use XSL transformation</td><td><input type="checkbox" name="usexslt" value="1"/></td></tr>
+<tr><td>XSLT File</td><td><input type="file" name="xslt_upload"/></td></tr>
+</table>
        <input type="submit" name="importProject" value="Import"/>
-       
+
        <input type="hidden" name="option" value="<?php echo JRequest::getVar( 'option' );?>"/>
        <input type="hidden" name="task" value="importProject"/> 
        <input type="hidden" name="hidemainmenu" value="0"/>  
